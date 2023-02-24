e8f08d4c76a835466864e4520ae220e1d626b84f
Earl Villapaz <jasonburning16@gmail.com>
2023-02-24 19:15:19 +0800

    second commit

---
+++ script/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        category: "Reaction",
+        score: 80,
+        icon: "./assets/images/icon-reaction.svg",
+    },
+    {
+        category: "Memory",
+        score: 92,
+        icon: "./assets/images/icon-reaction.svg",
+    },
+    {
+        category: "Verbal",
+        score: 61,
+        icon: "./assets/images/icon-reaction.svg",
+    },
+    {
+        category: "Visual",
+        score: 72,
+        icon: "./assets/images/icon-reaction.svg",
+    }
+];
+
+export default data;
